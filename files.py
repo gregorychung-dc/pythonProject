9b26a42af05e70a83b29fdb128174bd9f0a9a06c
gregorychung-dc <gregory.chung@deltacapita.com>
2022-12-21 16:52:09 +0000

    Fixing!

--- files.py
+++ files.py
@@ -4,21 +4,19 @@
 # the existing file if the file exists. If the file does
 # not exist, creates a new file for reading and writing.
 
-# writing files
-with open("story.txt", "w") as f:
+# writing files, using "with" will run .close() implicitly
+with open("story.txt", "w+", encoding="utf-8") as f:
     f.write("Little red riding hood was \n")
     f.write("walking in the woods. \n")
     f.write("She picked flowers on her way to see her grandmother. \n")
+    f.seek(0)           # reset cursor to start
+    print(f.read())     # .read returns specified number of bytes
 
-# opening files
+# try:
+#     story = open("./story.txt", "r")
+#     content = story.read()
+#     print(content)      # print the content
+# finally:                # if exception, will enforce .close()
+#     story.close()
 
-# file object/handler
-try:
-    story = open("./story.txt", "r")
-    content = story.read()
-    print(content)      # print the content
-finally:                # if exception, will enforce .close()
-    story.close()
 
-# using 'with', .close() will be run implicitly
-# with open("./fruits.txt", "r") as fruits:
